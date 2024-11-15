55305cb1e0c03a17b3433ccd8111c0d490e3393d
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-15 13:47:23 +0100

    feat: implementazione snack-6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,11 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = [];
+const zucchineCorte = [];
+
+zucchine.forEach((curItem, i) => lunghezza = (curItem.length >= 15) ? zucchineLunghe.push(curItem) : zucchineCorte.push(curItem))
+
+console.log(zucchineLunghe);
+console.log(zucchineCorte);
