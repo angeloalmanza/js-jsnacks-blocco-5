7ef180380a1c1b6c68ae6c38c7a74daf0814ee22
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-15 13:23:15 +0100

    feat: implementazione snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,14 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+let result = '';
+
+names.forEach((curItem, i) => {
+    result += `${curItem}`;
+    if(i < names.length - 1){
+        result += `, `;
+    }
+})
+
+console.log(result);
