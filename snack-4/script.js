a799d6f2a7dd82d8771db69e65186ca4780f309e
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-15 13:33:14 +0100

    feat: implementazione snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const newArray = posts.map(({author}, i) => {
+  return author;
+})
+
+console.log(newArray);
